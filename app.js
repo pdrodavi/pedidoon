542e44239b87b89947061ada7d26cdcbc9c4a02c
Pedro Davi <pdrodavi@outlook.com>
2021-04-24 12:09:28 -0300

    refactor structure

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const edge = require('edge.js')
 const path = require('path')
 const cors = require('cors');
 
+require('dotenv').config();
+
 // protect
 const helmet = require('helmet')
 const cookieParser = require('cookie-parser')
@@ -16,9 +18,11 @@ const app = express()
 
 // view engine setup
 edge.registerViews(path.join(__dirname, '/views'))
-app.use(express.static(__dirname + '/public/'))
 app.use(engine)
-app.set('views', __dirname + '/views')
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'edge')
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(cors())
 app.use(morgan('dev'))
@@ -27,12 +31,13 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
-    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP
-        res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS
-    else //Se a requisição já é HTTPS
-        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
-})
+
+//app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
+//    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP
+//        res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS
+//    else //Se a requisição já é HTTPS
+//        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
+//})
 
 // protect
 app.use(helmet());
@@ -55,7 +60,6 @@ app.use(function(req, res) {
     //res.render('404')
 })
 
-// start server
 app.listen(process.env.PORT || 3000, () => {
     logger.info(`Server listening on port ${process.env.PORT}`);
 })
